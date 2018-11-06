b0518bc76e1ade4dd1d5b95a2c0dc4d544f3ad2c
DusterTheFirst <dusterthefirst@users.noreply.github.com>
2018-11-06 13:35:16 -0500

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import docs from './doc.json';
+import './App.scss';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="app">
+                <pre><code>{JSON.stringify(docs, null, 4)}</code></pre>
+            </div>
+        );
+    }
 }
 
 export default App;
